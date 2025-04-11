bd2eb5363b16781268bd5b0a01f8d3a912bf3593
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-04-11 08:57:50 +0200

    add arithmetic operations

---
+++ datatypes_operators.js
@@ -0,0 +1,35 @@
+let num1 = 60;
+let num2 = 50.35;
+
+console.log("num1:", num1);
+console.log("num2:", num2);
+
+let num3 = 8;
+
+let sum = num1 + num3;
+console.log("sum:", sum);
+
+let difference = num1 - num3;
+console.log("difference:", difference);
+
+let product = num1 * num3;
+console.log("product:", product);
+
+let quotient = num1 / num3;
+console.log("quotient:", quotient);
+
+let remainder = num1 % num3;
+console.log("remainder:", remainder);
+
+let power = num1 ** num3;
+console.log("power:", power);
+
+console.log("num1:", num1);
+console.log("num2:", num2);
+console.log("num3:", num3);
+console.log("sum:", sum);
+console.log("difference:", difference);
+console.log("product:", product);
+console.log("quotient:", quotient);
+console.log("remainder:", remainder);
+console.log("power:", power);
