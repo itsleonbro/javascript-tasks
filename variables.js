7c6bedbd683845741b2ecb747dcd92f03e4afb15
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-04-11 08:49:02 +0200

    add variables and calculations with console logs

---
+++ variables.js
@@ -0,0 +1,24 @@
+let name = "Leon";
+let age = 22;
+let isStudent = false;
+
+console.log(name, age, isStudent);
+
+let favoriteColour = "yellow";
+console.log(favoriteColour);
+
+favoriteColour = "teal";
+console.log(favoriteColour);
+
+let num1 = 30;
+let num2 = 20;
+
+let sum = num1 + num2;
+let difference = num1 - num2;
+let product = num1 * num2;
+let quotient = num1 / num2;
+
+console.log("sum:", sum);
+console.log("difference:", difference);
+console.log("product:", product);
+console.log("quotient:", quotient);
