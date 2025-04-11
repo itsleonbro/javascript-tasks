3229cc3400b79bce64426c48e57718c43d925dcd
Leon Hlabathi <114271403+itsleonbro@users.noreply.github.com>
2025-04-11 09:07:42 +0200

    add numbers and boolean operators

--- datatypes_operators.js
+++ datatypes_operators.js
@@ -33,3 +33,64 @@ console.log("product:", product);
 console.log("quotient:", quotient);
 console.log("remainder:", remainder);
 console.log("power:", power);
+
+let num4 = 15;
+let num5 = 25;
+
+let isgreaterthan = num4 > num5;
+let islessthan = num4 < num5;
+let isgreaterorequal = num4 >= num5;
+let islessorequal = num4 <= num5;
+let isequal = num4 === num5;
+let isnotequal = num4 !== num5;
+
+console.log("comparing", num4, "and", num5);
+console.log("is greater than:", isgreaterthan);
+console.log("is less than:", islessthan);
+console.log("is greater than or equal to:", isgreaterorequal);
+console.log("is less than or equal to:", islessorequal);
+console.log("is equal to:", isequal);
+console.log("is not equal to:", isnotequal);
+
+let x = 8;
+let y = 12;
+
+let xgreaterthany = x > y;
+let xlessorequaltoy = x <= y;
+let xequaltoy = x === y;
+let xnotequaltoy = x !== y;
+
+console.log("\ncomparing x =", x, "and y =", y);
+console.log("x is greater than y:", xgreaterthany);
+console.log("x is less than or equal to y:", xlessorequaltoy);
+console.log("x is equal to y:", xequaltoy);
+console.log("x is not equal to y:", xnotequaltoy);
+
+let a = true;
+let b = false;
+
+let Aandb = a && b;
+let Aorb = a || b;
+let nota = !a;
+
+console.log("\nlogical operations");
+console.log("a and b:", Aandb);
+console.log("a or b:", Aorb);
+console.log("not a:", nota);
+
+let p = 10;
+
+p += 5;
+console.log("p += 5:", p);
+
+p -= 3;
+console.log("p -= 3:", p);
+
+p *= 2;
+console.log("p *= 2:", p);
+
+p /= 4;
+console.log("p /= 4:", p);
+
+p %= 3;
+console.log("p %= 3:", p);
